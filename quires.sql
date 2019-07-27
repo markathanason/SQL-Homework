6949fa5233386dfd32f6e9cbacea01e08e892928
Mark Athanason <mark.athanason@vumc.org>
2019-07-27 12:55:48 -0500

    Homework complete

--- quires.sql
+++ quires.sql
@@ -1,14 +1,63 @@
---List the following details of each employee: employee number, last name, first name, gender, and salary.
+--Use the information you have to create a table schema for each of the six CSV files. Remember to specify data types, primary keys, foreign keys, and other constraints.
+--Import each CSV file into the corresponding SQL table.
+
+CREATE TABLE departments (
+	dept_no varchar(10) NOT null PRIMARY KEY,
+	dept_name varchar(100)
+);
+
+CREATE TABLE employees (
+	emp_no int NOT null PRIMARY KEY,
+	birth_date date NOT null DEFAULT now(),
+	first_name varchar(75),
+	last_name varchar(75),
+	gender varchar(5),
+	hire_date date NOT null
+);
+
+CREATE TABLE dept_emp (
+	emp_no int NOT null REFERENCES employees(emp_no),
+	dept_no varchar(10) NOT null REFERENCES departments(dept_no),
+	from_date date NOT null,
+	to_date date NOT null
+);
+
+CREATE TABLE dept_manager (
+	emp_no int NOT null REFERENCES employees(emp_no),
+	dept_no varchar(10) NOT null REFERENCES departments(dept_no),
+	from_date date NOT null,
+	to_date date NOT null
+);
+
+CREATE TABLE salaries (
+	emp_no int NOT null REFERENCES employees(emp_no),
+	salary int,
+	from_date date NOT null,
+	to_date date NOT null
+);
+
+CREATE TABLE titles (
+	emp_no int NOT null REFERENCES employees(emp_no),
+	title varchar(75) NOT null,
+	from_date date NOT null,
+	to_date date NOT null
+);
+	
+--List the following details of each employee: 
+--employee number, last name, first name, gender, and salary.
+
 SELECT employees.emp_no, first_name, last_name, gender, salary
 FROM employees INNER JOIN salaries
 	ON salaries.emp_no = employees.emp_no
 ;
+
 --List employees who were hired in 1986.
 
 SELECT first_name, last_name, hire_date FROM employees WHERE hire_date >('1987/1/1') AND hire_date <('1987/12/31');
 
 --List the manager of each department with the following information: 
 --department number, department name, the manager's employee number, last name, first name, and start and end employment dates.
+
 SELECT dept_name, employees.emp_no, last_name, first_name, dept_manager.from_date, dept_manager.to_date
 FROM departments RIGHT JOIN dept_manager
 	ON departments.dept_no = dept_manager.dept_no
@@ -61,4 +110,3 @@ WHERE dept_name = 'Sales' OR dept_name = 'Development'
 SELECT last_name, count(last_name) FROM employees
 GROUP BY last_name ORDER BY count(last_name) desc
 ;
-
